678adc6a3512a82136b03e0ec835920a35aaced7
princy101999 <princyvirani101999@gmail.com>
2024-01-03 15:15:37 -0500

    add code of chintan

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+    (window.adsbygoogle = window.adsbygoogle || []).push({})
+    return(
+        <div>
+        <ins className = "adsbygoogle"
+                style = { {display:"inline-block",width:"728px",height:"90px"} }
+                data-ad-client = "ca-pub-114234437311277"
+                data-ad-slot = "42837282224"></ins>
+        </div>)
+}
+
+export default App;
